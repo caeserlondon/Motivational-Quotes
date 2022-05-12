25df27e35b1533d625dafa90597776c097cd010b
Caeser F Ibrahim (he/him) <caeserlondon@gmail.com>
2022-05-12 23:49:16 +0700

    Connecting the handlebars to the App

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
 const connectDB = require('./config/db.js');
+import { engine } from 'express-handlebars';
 
 //loading the configiration
 dotenv.config({ path: './config/config.env' });
@@ -11,9 +12,15 @@ connectDB();
 
 const app = express();
 
+// loggin with morga
 if (process.env.NODE_ENV === 'development') {
 	app.use(morgan('dev'));
 }
+
+// handlebars
+app.engine('.hbs', engine({ defaultLayout: 'main', extname: '.hbs' }));
+app.set('view engine', '.hbs');
+app.set('views', './views');
 
 const PORT = process.env.PORT || 3030;
 
