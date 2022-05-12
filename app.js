fd914c9d337b3b669697a456db63baa608536a2f
Caeser F Ibrahim (he/him) <caeserlondon@gmail.com>
2022-05-12 23:32:27 +0700

    Connecting the Morgan middleware to the App

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const morgan = require('morgan');
 const connectDB = require('./config/db.js');
 
 //loading the configiration
@@ -9,6 +10,10 @@ dotenv.config({ path: './config/config.env' });
 connectDB();
 
 const app = express();
+
+if (process.env.NODE_ENV === 'development') {
+	app.use(morgan('dev'));
+}
 
 const PORT = process.env.PORT || 3030;
 
