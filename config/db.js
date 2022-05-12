30e8d0a1c4f6c5630e5cac3c2acb3101d4246aba
Caeser F Ibrahim (he/him) <caeserlondon@gmail.com>
2022-05-12 23:24:21 +0700

    Updating the db.js

--- config/db.js
+++ config/db.js
@@ -2,15 +2,11 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
 	try {
-		const conn = await mongoose.connect(process.env.MONGO_URI, {
-			useNewUrlParser: true,
-			useUnifiedTopology: true,
-			useFindAndModify: false,
-		});
+		const conn = await mongoose.connect(process.env.MONGO_URI, {});
 
 		console.log(`MongoDB Connected: ${conn.connection.host}`);
 	} catch (error) {
-		console.error(err);
+		console.error(error);
 		process.exit(1);
 	}
 };
