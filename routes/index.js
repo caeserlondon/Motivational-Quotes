f236a52c26b17b5eaa985ce6f132caefdba36753
Caeser F Ibrahim (he/him) <caeserlondon@gmail.com>
2022-05-13 08:57:00 +0700

    Creating the Routes index.js

---
+++ routes/index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const router = express.Router();
+
+// @desc login/landing page
+// @route GET/
+
+router.get('/', (req, res) => {
+	res.render('login');
+});
+
+// @desc Dashboard
+// @route GET/dashboard
+
+router.get('/dashboard', (req, res) => {
+	res.render('dashboard');
+});
+
+module.exports = router;
