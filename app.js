ad2bb4f3210a30acfa13ea38844392d3a5b274eb
Caeser F Ibrahim (he/him) <caeserlondon@gmail.com>
2022-05-14 15:30:11 +0700

    Creating and configuring the Sessions and the passport middleware

--- app.js
+++ app.js
@@ -2,32 +2,54 @@ const path = require('path');
 const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
-const connectDB = require('./config/db.js');
 const { engine } = require('express-handlebars');
+// const exphbs = require('express-handlebars');
 
-//loading the configiration
+const passport = require('passport');
+const session = require('express-session');
+const connectDB = require('./config/db.js');
+
+//Loading the configiration
 dotenv.config({ path: './config/config.env' });
+
+// Passport config
+require('/config/passport')(passport);
 
 //making the connection
 connectDB();
 
 const app = express();
 
-// loggin with morga
+// Loggin with morga
 if (process.env.NODE_ENV === 'development') {
 	app.use(morgan('dev'));
 }
 
-// handlebars
+// Handlebars
 app.engine('.hbs', engine({ defaultLayout: 'main', extname: '.hbs' }));
 app.set('view engine', '.hbs');
 app.set('views', './views');
 
-// static folder
+// Sessions
+// app.set('trust proxy', 1); // trust first proxy
+app.use(
+	session({
+		secret: 'keyboard cat',
+		resave: false,
+		saveUninitialized: false,
+	})
+);
+
+//Passport middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Static folder
 app.use(express.static(path.join(__dirname, 'public')));
 
 //Routes
 app.use('/', require('./routes/index'));
+app.use('/auth', require('./routes/auth'));
 
 // PORT
 const PORT = process.env.PORT || 3030;
