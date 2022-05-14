34d5932f7f5dacc9c707bd8131060be10e6ba5d0
Caeser F Ibrahim (he/him) <caeserlondon@gmail.com>
2022-05-14 19:40:17 +0700

    Configuring the newUser for the google strategy

--- config/passport.js
+++ config/passport.js
@@ -2,28 +2,46 @@ const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const mongoose = require('mongoose');
 const User = require('../models/User');
 
-modules.exports = function (passport) {
+module.exports = function (passport) {
 	passport.use(
 		new GoogleStrategy(
 			{
-				clientID: process.env.GOOGLE_CLINT_ID,
+				clientID: process.env.GOOGLE_CLIENT_ID,
 				clientSecret: process.env.GOOGLE_CLIENT_SECRET,
 				callbackURL: '/auth/google/callback',
 			},
-			async (accessToken, refreshToken, profile, dune) => {
-				console.log(profile);
+			async (accessToken, refreshToken, profile, done) => {
+				const newUser = {
+					googleId: profile.id,
+					displayName: profile.displayName,
+					firstName: profile.name.givenName,
+					lastName: profile.name.familyName,
+					image: profile.photos[0].value,
+				};
+
+				try {
+					let user = await User.findOne({ googleId: profile.id });
+
+					if (user) {
+						done(null, user);
+					} else {
+						user = await User.create(newUser);
+						done(null, user);
+					}
+				} catch (err) {
+					console.error(err);
+				}
 			}
 		)
 	);
+
 	// used to serialize the user for the session
-	passport.serializeUser(function (user, done) {
+	passport.serializeUser((user, done) => {
 		done(null, user.id);
 	});
 
 	// used to deserialize the user
-	passport.deserializeUser(function (id, done) {
-		User.findById(id, function (err, user) {
-			done(err, user);
-		});
+	passport.deserializeUser((id, done) => {
+		User.findById(id, (err, user) => done(err, user));
 	});
 };
