462d70c3e23dc8b688cabd74fd9cb66c1e96b878
Caeser F Ibrahim (he/him) <caeserlondon@gmail.com>
2022-05-14 19:38:38 +0700

    Configuring the Logout router function

--- routes/auth.js
+++ routes/auth.js
@@ -2,13 +2,12 @@ const express = require('express');
 const passport = require('passport');
 const router = express.Router();
 
-// @desc Auth with Google
-// @route GET  /auth/google
-
+// @desc    Auth with Google
+// @route   GET /auth/google
 router.get('/google', passport.authenticate('google', { scope: ['profile'] }));
 
-// @desc Google auth callback
-// @route GET  /auth/google/callback
+// @desc    Google auth callback
+// @route   GET /auth/google/callback
 router.get(
 	'/google/callback',
 	passport.authenticate('google', { failureRedirect: '/' }),
@@ -16,5 +15,12 @@ router.get(
 		res.redirect('/dashboard');
 	}
 );
+
+// @desc    Logout user
+// @route   /auth/logout
+router.get('/logout', (req, res) => {
+	req.logout();
+	res.redirect('/');
+});
 
 module.exports = router;
